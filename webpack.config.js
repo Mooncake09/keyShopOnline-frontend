5935815ba10a13cebaa4c859e4d6b04933851224
Zakhar Vislov <tabak1805@gmail.com>
2022-03-06 15:02:11 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+   });
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("dist"),
+    publicPath: "/",
+  },
+  devServer: {
+    server: 'http',
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9090,
+    proxy: {
+      '/api': "http://localhost:9000"
+    },
+    historyApiFallback: true
+  },
+  module: {
+    rules:[
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+    ], 
+  },
+  plugins: [htmlPlugin]
+}
